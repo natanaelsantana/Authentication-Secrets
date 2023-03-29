2b2cf6d5e0af7e01452fd0c3322f2b9718181f23
Natanael <natanael.santana@outlook.com.br>
2023-03-29 17:20:05 -0300

    dotenv added, one level more of security increased

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
@@ -21,8 +22,7 @@ const userSchema = new mongoose.Schema ({
 })
 
 //Use for encryptation
-const secret ="Thisisourlittlescret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']})
+userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ['password']})
 
 
 const User = new mongoose.model("User", userSchema)
