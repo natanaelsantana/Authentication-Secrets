291fda6d6a3aaedd459e9e14ba3f7c2dabccdbf5
Natanael <natanael.santana@outlook.com.br>
2023-03-31 17:34:17 -0300

    Passport added, now the user can stay logged as long he request a logOut or server restart. Passport does salt and hash automatically, one lvl security added

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require ("mongoose");
-const bcrypt = require ("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport')
+const passportLocalMongoose = require("passport-local-mongoose");
+const { application } = require('express');
+
 
 
 const app = express();
@@ -16,6 +19,16 @@ app.use(bodyParser.urlencoded ({
     extended:true
 }));
 
+app.use(session({
+    secret: 'Our little secret',
+    resave: false,
+    saveUninitialized: false,
+    //cookie: { secure: true }
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
@@ -23,10 +36,21 @@ const userSchema = new mongoose.Schema ({
     password:String 
 })
 
-//Use for encryptation
+//passport already set hash, thats why we excluded md5
+userSchema.plugin(passportLocalMongoose);
 
 
 const User = new mongoose.model("User", userSchema)
+
+//this configure passport to use a strategy function to authenticate User
+passport.use(User.createStrategy());
+
+/*This two lines keeps the user authenticated during the session
+turning possible to access entire website without login again and keeping him logged
+until he log out*/
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function (req, res) {
     res.render("home")
@@ -39,42 +63,60 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
     res.render("register")
+
 })
+
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated ()) {
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
+});
+
+/*Passport oficial site say to use "POST" instead "GET" in order to prevent some accidental 
+log out request, but it doesn't function propely, so i used "GET"*/
+app.get('/logout', function(req, res, next){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+        });
+});
+
 
 app.post("/register", function (req,res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User ({
-            email: req.body.username,
-            password: hash
-        })
-        newUser.save()
-                .then(function () {
-                    res.render("secrets")
-                    })
+    User.register({username: req.body.username}, req.body.password, function (err,user){
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req,res, function () {
+                res.redirect("/secrets")
+            });
+        };
     });
 
    
 })
 
 app.post("/login", function (req,res) {
-    const username = (req.body.username)
-    const password = (req.body.password)
-      
-        User.findOne({ email: username }).exec()
-          .then(function(foundUser) {
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-            if (result === true) {
-              res.render("secrets");
-            } else {
-              res.send("User or password incorrect", err);
-            } });
-          })
-          .catch(function(err) {
-            console.error("Login failed, try again later", err);
-          });
+    const user = new User ({
+        username : req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req,res, function () {
+                res.redirect("/secrets")
+        })
+    }
+
       });
-      
+})
 
 
 app.listen(3000,function() {
