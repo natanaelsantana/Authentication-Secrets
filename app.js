0c18bd329414336effe566816d72385b2400d8dd
Natanael <natanael.santana@outlook.com.br>
2023-03-31 16:12:45 -0300

    Salt and Hash method added, one more level of security added

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
-const mongoose = require ("mongoose")
-const md5 = require("md5")
+const mongoose = require ("mongoose");
+const bcrypt = require ("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.use(bodyParser.urlencoded ({
     extended:true
 }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema ({
     email: String,
@@ -40,27 +42,33 @@ app.get("/register", function (req, res) {
 })
 
 app.post("/register", function (req,res) {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save()
-            .then(function () {
-                res.render("secrets")
-                })
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save()
+                .then(function () {
+                    res.render("secrets")
+                    })
+    });
+
+   
 })
 
 app.post("/login", function (req,res) {
     const username = (req.body.username)
-    const password = md5(req.body.password)
+    const password = (req.body.password)
       
         User.findOne({ email: username }).exec()
           .then(function(foundUser) {
-            if (foundUser && foundUser.password === password) {
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
               res.render("secrets");
             } else {
-              res.send("User or password incorrect");
-            }
+              res.send("User or password incorrect", err);
+            } });
           })
           .catch(function(err) {
             console.error("Login failed, try again later", err);
